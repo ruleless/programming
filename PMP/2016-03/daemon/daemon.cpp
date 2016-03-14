7469e1d1d1554976624c0d80730a004dfae53beb
ruleless <ruleless@126.com>
2016-03-14 23:58:51 +0800

    The Procedure to create daemon process:1.fork and exit parent process 2. create session 3. dup all default fds(0,1,2) to /dev/null

---
+++ PMP/2016-03/daemon/daemon.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+int daemon()
+{
+	// fork process
+    switch (fork())
+	{
+    case -1:
+		fputs("fork failed!\n", stderr);
+        return -1;
+    case 0:
+        break;
+    default:
+		exit(0);
+    }
+
+	// create session
+    if (setsid() == -1)
+	{
+        return -1;
+    }
+
+	// dup stdin/stdout/stderr to null
+    umask(0);
+
+    int fd = open("/dev/null", O_RDWR);
+    if (fd == -1)
+	{        
+        return -1;
+    }
+
+    if (dup2(fd, STDIN_FILENO) == -1)
+	{
+        return -1;
+    }
+
+    if (dup2(fd, STDOUT_FILENO) == -1)
+	{
+        return -1;
+    }
+
+#if 0
+    if (dup2(fd, STDERR_FILENO) == -1)
+	{
+        return -1;
+    }
+#endif
+
+    if (fd > STDERR_FILENO)
+	{
+        if (close(fd) == -1)
+		{
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	daemon();
+	
+	pid_t pid = getpid();
+	char log[256] = {0};
+	while (1)
+	{
+		snprintf(log, sizeof(log), "pid=%d  ppid=%d\n", pid, getppid());
+		fputs(log, stderr);
+		sleep(1);
+	}
+    return 0;
+}
+
