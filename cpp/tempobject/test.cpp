2c63eaeff41af11b104cb80d789ea25f53590d10
ruleless <ruleless@126.com>
2016-06-25 11:21:14 +0800

    modify c++ tempobj test

--- cpp/tempobject/test.cpp
+++ cpp/tempobject/test.cpp
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-//--------------------------------------------------------------------------
-namespace situation1
-{
 class Obj
 {
   public:
@@ -13,16 +10,44 @@ class Obj
 		strncpy(mDesc, desc, 256);
 		printf("default construct %p \"%s\"\n", (void *)this, mDesc);
 	}
+
+	Obj(const Obj &o)
+	{
+		strncpy(mDesc, o.mDesc, 256);
+		printf("copy construct %p \"%s\"\n", (void *)this, mDesc);
+	}
+
+	Obj& operator= (const Obj &o)
+	{
+		strncpy(mDesc, o.mDesc, 256);
+		printf("operator= %p \"%s\"\n", (void *)this, mDesc);
+		return *this;
+	}
+	
     virtual ~Obj()
 	{
 		printf("~ destruct %p \"%s\"\n", (void *)this, mDesc);
-	}
+	}	
   private:
 	char mDesc[256];
 };
 
+//--------------------------------------------------------------------------
+// Situation 1.
+namespace situation1
+{
 void testFunc1(const Obj &i)
 {	
+}
+}
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+// Situation 2.
+namespace situation2
+{
+void testFunc1(Obj i)
+{
 }
 }
 //--------------------------------------------------------------------------
@@ -31,50 +56,15 @@ void testFunc1(const Obj &i)
 // Situation 3.
 namespace situation3
 {
-class Obj
-{
-  public:
-    Obj(int i = 0) : a(i)
-	{
-		printf("default construct %p %d\n", (void *)this, a);
-	}
-
-	Obj(const Obj &obj)
-	{
-		a = obj.a;
-		printf("copy construct %p %d\n", (void *)this, a);
-	}
-
-	Obj& operator= (const Obj &o)
-	{
-		a = o.a;
-		printf("operator= %p %d\n", (void *)this, a);
-		return *this;
-	}
-	
-    virtual ~Obj()
-	{
-		printf("~ destruct %p %d\n", (void *)this, a);
-	}
-
-	void set(int i)
-	{
-		a = i;
-	}
-  private:
-	int a;
-};
-
 Obj testFunc1()
 {
-	Obj o(1);
-	o.set(1);
+	Obj o("test1");
 	return o;
 }
 
 Obj testFunc2()
 {
-	return Obj(2); // it's called RVO(Return Value Optimization)
+	return Obj("test2"); // it's called RVO(Return Value Optimization)
 }
 }
 //--------------------------------------------------------------------------
@@ -89,10 +79,20 @@ int main(int argc, char *argv[])
 	}
 	puts("");
 	{
+		printf("situation 2 begin\n");
+		using namespace situation2;
+		Obj o("situation 2");
+		testFunc1(o);
+		printf("situation 2 end\n");
+	}
+	puts("");
+	{
 		printf("situation 3 begin\n");
 		using namespace situation3;
 		Obj o1 = testFunc1();
 		Obj o2 = testFunc2();
+		Obj o3 = o1;
+		o3 = o2;
 		printf("situation 3 end\n");
 	}
 #ifndef unix
