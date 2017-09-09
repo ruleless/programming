bfe2bc3a0bac0ade44f34417ba2a1d664a11d85a
ruleless <ruleless@126.com>
2017-09-09 22:35:27 +0800

    value-exp

--- shell/basic/value-exp.sh
+++ shell/basic/value-exp.sh
@@ -7,8 +7,8 @@ echo '$@':$@
 echo '$#':$#
 
 arg1=$1
-echo ${arg1:-default}
-# echo ${arg1:=default}
-# echo ${arg1:?"undefined"}
-echo ${arg1:+default}
+# echo '${arg1:-default}='${arg1:-default}
+echo '${arg1:=default}='${arg1:=default}
+echo '${arg1:?"undefined"}='${arg1:?"undefined"}
+# echo '${arg1:+default}='${arg1:+default}
 echo ${arg1:1}
